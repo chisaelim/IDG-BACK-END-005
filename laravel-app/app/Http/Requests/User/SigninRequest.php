21c40562796afd70295dab9354fc5784b448f83b
chisaelim <chisae0123@gmail.com>
2026-07-31 10:13:06 +0700

    change email signin key to user_email

--- laravel-app/app/Http/Requests/User/SigninRequest.php
+++ laravel-app/app/Http/Requests/User/SigninRequest.php
@@ -23,7 +23,7 @@ class SigninRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|exists:users,email',
+            'user_email' => 'required|email|exists:users,email',
             'password' => 'required|string|min:6|max:10'
         ];
     }
